63f1ad87e062917d64badf252a093fe4da6b3fce
regship-yandex <91245488+regship-yandex@users.noreply.github.com>
2021-09-27 21:04:24 +0300

    Update Задание 3.py

--- Урок 5/Задание 3.py
+++ Урок 5/Задание 3.py
@@ -6,16 +6,16 @@ right_data = ticket % 1000
 left_data1 = left_data // 100
 left_data2 = left_data % 100 //10
 left_data3 = left_data % 10
-leftt_summ = left_data1 + left_data2 +left_data3
-leftt_summ = left_data1 + left_data2 +left_data3
+left_summ = left_data1 + left_data2 +left_data3
+left_summ = left_data1 + left_data2 +left_data3
 
 right_data1 = right_data // 100
 right_data2 = right_data % 100 //10
 right_data3 = right_data % 10
 right_summ = right_data1 + right_data2 +right_data3
 
-if leftt_summ == right_summ:
+if left_summ == right_summ:
     print('Билет', ticket, 'счастливый')
 
-eleftt_summe:
+else:
     print('Билет', ticket, 'Несчастливый')
