95669c6178189f2640e028f609e34a6760861e1e
regship-yandex <91245488+regship-yandex@users.noreply.github.com>
2021-10-04 22:01:50 +0300

    Create Задание 7.py

---
+++ Урок 10/Задание 7.py
@@ -0,0 +1,6 @@
+last_name = input('Введите Фамилию: ')
+name = input('Введите Имя: ')
+patronic = input('Введите Отчество: ')
+name = name[0] +'.'
+patronic = patronic[0] + '.'
+print(last_name, name, patronic)
