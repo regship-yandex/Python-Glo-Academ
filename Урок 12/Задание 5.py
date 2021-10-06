4ace6b3769e8cf963ffcd7b72366a2a7b7f121f3
regship-yandex <91245488+regship-yandex@users.noreply.github.com>
2021-10-06 17:53:36 +0300

    Update Задание 5.py

--- Урок 12/Задание 5.py
+++ Урок 12/Задание 5.py
@@ -1,7 +1,8 @@
-data_gold = input()
-data_gold = data_gold.split('.')
-
-data1 = int(data_gold[0])
-data2 = int(data_gold[1])
-composition = int(data_gold[2]) % 100
-print(data1*data2 == composition)
+def gold_data(data_gold):
+    data_gold = data_gold.split('.')
+    data1 = int(data_gold[0])
+    data2 = int(data_gold[1])
+    composition = int(data_gold[2]) % 100
+    print(data1*data2 == composition)
+data_g = input()
+gold_data(data_g)
