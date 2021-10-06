12655d4ae5aee3d241ba7b8bef6367e92d0eeab3
regship-yandex <91245488+regship-yandex@users.noreply.github.com>
2021-10-06 17:08:03 +0300

    Update Задание 10.py

--- Урок 10/Задание 10.py
+++ Урок 10/Задание 10.py
@@ -1,10 +1,2 @@
 line = input()
-line_len = len(line)
-count_line = 1
-for i in range(line_len):
-    code = ord(line[i])
-    if code == 32:
-        count_line += 1
-if count_line <= 1:
-    count_line = 0
-print(count_line)
+print(len(line.split()))
