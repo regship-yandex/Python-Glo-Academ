d9e6b89aefbbced1ffaf46c5dcf38a6c8d0b376f
regship-yandex <91245488+regship-yandex@users.noreply.github.com>
2021-10-09 10:54:37 +0300

    Add files via upload

--- Урок  13/Задание 1.py
+++ Урок  13/Задание 1.py
@@ -5,7 +5,7 @@
 # Если загаданное число меньше чем введенное число-меньше
 # Если загаданное число равно введенному числу - Победа!
 # До новых встреч
-
+import math
 from random import *    # библиотека одна
 def is_valid(user_input, max):
     if user_input.isdigit():
@@ -19,6 +19,7 @@ def sold_out ():
     secret_number = 0
     max = 100
     while True:
+        #print('Хотите продолжить? Введите [y]или[n]')
         print('Начать Игру? Введите [y]или[n]')
         answer = input()
         if not (answer == 'y' or answer == 'н'):
@@ -26,6 +27,7 @@ def sold_out ():
 
         max = int(input("max правую границу "))
         secret_number = randint(1, max)
+        # print('я загодал число от 1 до ', max ,"по секрету " , secret_number)
         print('я загодал число от 1 до', max)
         main_function(secret_number, max)
     print('Пока пока...')
@@ -46,10 +48,13 @@ def main_function(secret_number, max):
         elif secret_number < user_number:
             print('Загаданное число меньше, чем введенное вами число')
         else:
-            print('Ура! Вы угудали число за', total, 'ходов!')
+            print('Ура! Вы угудали число за', total, 'ходов!\nОптимальное число ходов равно:')
+            print(int(math.log2(max)), 'ходов')
             print('Раунд окончен')
             break
 
 
+#secrert_number = randint(1, 100)
 print('Добро пожаловать в игру "Угадай число"')
 sold_out ()
+#main_function(secrert_number, total)
